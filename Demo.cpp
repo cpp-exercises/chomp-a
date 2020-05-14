6dc53b8f7564c8ea7ed39b8f4fd4e7339758a3ba
Erel Segal-Halevi cron <erelsgl@gmail.com>
2020-05-14 20:29:28 +0300

    Update the demo

--- Demo.cpp
+++ Demo.cpp
@@ -7,15 +7,17 @@
 
 #include <iostream>
 #include <complex>
-#include "play.hpp"
+#include <cstdlib>
+#include "play.hpp"     // You have to write this file
+#include "Winner.hpp"   // You have to write this file
 #include "DemoPlayers.hpp"
 
 using namespace std;
 
 int main() {
     chomp::HorizontalEater horizontal1("1"), horizontal2("2");
-    chomp::Player& winner = chomp::play(7, 4, horizontal1, horizontal2);
-    cout << winner << endl;  // HorizontalEater[1]
+    chomp::Player& the_winner = chomp::play(7, 4, horizontal1, horizontal2);
+    cout << the_winner << endl;  // HorizontalEater[1]
 
     chomp::Loser loser;
     cout << chomp::play(7, 4, horizontal1, loser) << endl;  // HorizontalEater[1]
@@ -29,6 +31,16 @@ int main() {
     chomp::VerticalEater vertical1("1");
     cout << chomp::play(7, 4, horizontal1, vertical1) << endl;  // HorizontalEater[1]
     cout << chomp::play(7, 4, vertical1, horizontal1) << endl;  // VerticalEater[1]
+
+    chomp::Winner winner("Winner");  // This is a class you have to write!
+    for (uint i=0; i<100; ++i) {
+        uint rows = rand() % 20 + 5;
+        uint cols = rand() % 20 + 5;
+        cout << chomp::play(rows, cols, winner, horizontal1) << endl;  // Winner
+        cout << chomp::play(rows, cols, horizontal1, winner) << endl;  // Winner
+        cout << chomp::play(rows, cols, winner, vertical1) << endl;  // Winner
+        cout << chomp::play(rows, cols, vertical1, winner) << endl;  // Winner
+    }
 
     return 0;
 }
--- Player.cpp
+++
@@ -1,8 +0,0 @@
-#include "Player.hpp"
-
-namespace chomp {
-	std::ostream& operator<< (std::ostream& out, const Player& player)  {
-		out << player.name();
-		return out;
-	}
-}
